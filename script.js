489d14d3ab79cadd05414b9b94ebacf8feac72ba
Nguyensoncoding <149793026+Nguyensoncoding@users.noreply.github.com>
2025-03-07 19:21:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function selectOption(option) {
         });
     } else if (option === 'no') {
         // Change text on the "No" button to "You sure?"
-        document.getElementById('no-button').innerText = 'chọn sai kìa'; 
+        document.getElementById('no-button').innerText = 'sai òi'; 
         // Increase font size of "Yes" button
         var yesButton = document.getElementById('yes-button');
         var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
